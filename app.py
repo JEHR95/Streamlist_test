11500a38c5847f4c98b738f33b3d24850ba73017
JEHR95 <jhernandez260895@gmail.com>
2025-09-03 17:42:38 -0600

    incorporación de variable "chart" para el gráfico de líneas y la función 'toss_coin' para emular lanzamiento de moneda n veces calculando la media por cada iteración

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
+#importación de librerías
 import streamlit as st
+import scipy.stats
+import time
 
+# Agrega un encabezado a la página
 st.header('Lanzar una moneda')
 
+chart = st.line_chart([0.5])
+
+#función que emula el lanzamiento de una moneda 
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count +=1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+# Genera un slider para determinar el número de lanzamientos de la moneda
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+# Crea un botón de ejecución
 start_button = st.button('Ejecutar')
 
 if start_button:
